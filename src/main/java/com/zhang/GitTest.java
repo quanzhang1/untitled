c21fa09c1cfe84b469228ea50465bd3699946293
quan <quan@163.com>
2022-11-29 11:19:13 +0800

    push commit

--- src/main/java/com/zhang/GitTest.java
+++ src/main/java/com/zhang/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello git-git4!");
         System.out.println("master commit");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
