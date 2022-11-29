52d8a93c79b3910e603971ef65eb56f4cb6785ab
quan <quan@163.com>
2022-11-29 09:44:39 +0800

    first commit

---
+++ src/main/java/com/zhang/GitTest.java
@@ -0,0 +1,11 @@
+package com.zhang;
+
+/**
+ * @author 张波
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
