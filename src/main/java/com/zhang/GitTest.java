4a3f3a6e7b52dd266939c1f0890eb604f8c20729
quanzhang1 <119305334+quanzhang1@users.noreply.github.com>
2022-11-29 15:47:00 +0800

    pull commit

--- src/main/java/com/zhang/GitTest.java
+++ src/main/java/com/zhang/GitTest.java
@@ -12,6 +12,6 @@ public class GitTest {
         System.out.println("hello git-git4!");
         System.out.println("master commit");
         System.out.println("hot-fix test");
-        System.out.println("push test");
+        System.out.println("pull test");
     }
 }
